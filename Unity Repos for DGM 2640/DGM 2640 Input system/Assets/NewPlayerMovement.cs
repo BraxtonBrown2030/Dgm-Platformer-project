67edb17a0bf39d8f9f8454e38d89281309c3d1b2
Braxton Brown <10944268@uvu.edu>
2023-08-28 22:16:58 -0600

    Creation and input system change

---
+++ Unity Repos for DGM 2640/DGM 2640 Input system/Assets/NewPlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerMovement : MonoBehaviour
+{
+    
+    PlayerInput playerinput;
+
+    void OnAwake()
+    {
+
+        
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity Repos for DGM 2640/DGM 2640 Input system/Assets/SoPLayerHealth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SoPLayerHealth : ScriptableObject
+{
+    public int health;
+
+    public void OnAwake()
+    {
+
+        
+
+    }
+
+
+
+
+
+
+
+}
